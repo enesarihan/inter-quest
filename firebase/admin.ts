116f4cef7896171c0b7bb911e9bcd5411256b20f
enesarihan <enessarihan35@icloud.com>
2025-04-07 22:47:38 +0300

    fix: correct typo in clientEmail environment variable in Firebase admin initialization

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -9,7 +9,7 @@ const initFirebaseAdmin = () => {
     initializeApp({
       credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIl,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       }),
     });
