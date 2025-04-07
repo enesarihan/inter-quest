5e9fa1699815bc61df3a8ea847a925a6cfd5c412
enesarihan <enessarihan35@icloud.com>
2025-04-07 22:39:16 +0300

    feat: implement interview generation API and enhance Agent component

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from "@/components/Agent";
+
+const Interview = () => {
+  return (
+    <>
+      <h3>Interview Generation</h3>
+
+      <Agent userName="You" userId="userId" type="generate" />
+    </>
+  );
+};
+
+export default Interview;
